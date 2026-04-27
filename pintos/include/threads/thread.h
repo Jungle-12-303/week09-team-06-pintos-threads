c45ae6d933cd0a3936cb66344e9f760d311274de
ummfieg <ummfieg@naver.com>
2026-04-27 17:02:28 +0900

    thread.h 파일 수정

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -108,7 +108,6 @@ struct thread {
 	/* Owned by thread.c. */
 	struct intr_frame tf;               /* Information for switching */
 	unsigned magic;                     /* Detects stack overflow. */
-	int64_t wake_tick;
 };
 
 /* If false (default), use round-robin scheduler.
